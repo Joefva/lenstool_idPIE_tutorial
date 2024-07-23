e1538a0661cf814cef38bb991e2bb2e6974ec25d
Joseph Allingham <joefva@gmail.com>
2024-07-23 17:28:54 +0300

    Try again

--- .bashrc
+++ .bashrc
@@ -3,5 +3,10 @@ export LENSTOOL_JA_DIR=/home/joseph/Software/Lenstool_JA
 export LD_LIBRARY_PATH=$LENSTOOL_JA_DIR/lib:$LD_LIBRARY_PATH
 export PYTHONPATH="${PYTHONPATH}:${LENSTOOL_JA_DIR}"
 
-export CODESDIR=/home/joseph/Work/Codes
-export PYTHONPATH="${PYTHONPATH}:${CODESDIR}"
+#export CODESDIR=/home/joseph/Work/Codes
+#export PYTHONPATH="${PYTHONPATH}:${CODESDIR}"
+
+export CFITSIO_PATH=/home/joseph/Software/cfitsio
+export PATH=$PATH:$CFITSIO_PATH:
+export LD_LIBRARY_PATH=$CFITSIO_PATH/lib/:$LD_LIBRARY_PATH
+export INCLUDE=$CFITSIO_PATH/include/:$INCLUDE
--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -1,14 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# This file only contains a selection of the most common options. For a full list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# If extensions (or modules to document with autodoc) are in another directory, add these directories to sys.path here. If the directory is relative to the documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
@@ -20,14 +17,6 @@ try:
     sys.path.insert(0, os.path.abspath(pythonpath))
 except:
     print("WARNING: No python path found!")
-
-
-sys.path.insert(0, os.path.abspath('/home/joseph/Work/cocococ'))
-
-
-sys.path.insert(0, os.path.abspath('/home/joseph/Work/Codes'))
-import astro_constants_mod
-print('astro_constants_mod.c = ', astro_constants_mod.c)
 
 
 try:
@@ -64,9 +53,7 @@ print("VERSION: " + version)
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings. They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
